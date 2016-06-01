5b30972ad8195424cbd5a02dc6c68620a8d5cfeb
Ashley Davis <ashley.davis@realseriousgames.com>
2016-06-01 15:18:19 +1000

    Added an example of hierarchy injection using interfaces.

---
+++ Assets/7. Hierarchy injection - by interface/ExampleDependencyWithInterface.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// An example of a dependency that implements an interface.
+/// </summary>
+public class ExampleDependencyWithInterface : MonoBehaviour, IExampleDependencyInterface
+{
+    // Use this for initialization
+    void Start ()
+    {	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {	
+	}
+
+    public void SomeUsefulFunction()
+    {
+        Debug.Log("This should really do something useful.");
+    }
+}
---
+++ Assets/7. Hierarchy injection - by interface/IExampleDependencyInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// An example interface for an injected depenency.
+/// </summary>
+public interface IExampleDependencyInterface
+{
+    // Add interface functions here.
+    void SomeUsefulFunction();
+}
+
