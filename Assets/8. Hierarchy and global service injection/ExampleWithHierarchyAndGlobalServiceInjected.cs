c90b6e10db12479d7aa695b8924bd72cd4c84946
Ashley Davis <ashley.davis@realseriousgames.com>
2016-06-08 11:36:24 +1000

    New combined example of both hierarchy and global service injection.

---
+++ Assets/8. Hierarchy and global service injection/ExampleWithHierarchyAndGlobalServiceInjected.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// An example MonoBehaviour that has a global service injected.
+/// </summary>
+public class ExampleWithHierarchyAndGlobalServiceInjected : MonoBehaviour
+{
+    [Inject]
+    public ExampleDependency ExampleDependency { get; set; }
+
+    [Inject]
+    public ExampleGlobalService ExampleGlobalService { get; set; }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
