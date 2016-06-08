44403724f2aadc0f77253da4877b319fd1f887ca
Ashley Davis <ashley.davis@realseriousgames.com>
2016-06-08 11:42:09 +1000

    Example that demonstrates how to polymorphically override a service with a hierarchy dependency.

---
+++ Assets/11. Hierarchy overrides global service/ExampleGlobalServiceThatImplementsInterface.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Service]
+public class ExampleGlobalServiceThatImplementsInterface : MonoBehaviour, IExampleDependencyInterface
+{
+    public void SomeUsefulFunction()
+    {
+        // Do something useful ...
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
