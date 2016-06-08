1920fe586d5ac8bbf8ecbcc0539320e050d32ee3
Ashley Davis <ashley.davis@realseriousgames.com>
2016-06-08 11:30:01 +1000

    Added a new example of a polymorphic dependency override in the hierarchy.

---
+++ Assets/9. Hierarchy injection - polymorphic override by interface/AnotherExampleDependencyWithInterface.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AnotherExampleDependencyWithInterface : MonoBehaviour, IExampleDependencyInterface
+{
+    public void SomeUsefulFunction()
+    {
+        // Do something useful.
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
